6a8471138dd4e2a3916a12c6362ebfe30d6f06cb
boreto08 <boris.botev97@gmail.com>
2018-05-22 14:51:20 +0300

    Blog completed

---
+++ Blog/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,224 @@
+﻿namespace Blog.Controllers
+{
+    using Blog.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: Article/List
+        public ActionResult List()
+        {
+            using (var db = new BlogDbContext())
+            {
+                var articles = db.Articles
+                    .Include(a => a.Author)
+                    .ToList();
+
+                return View(articles);
+            }
+
+        }
+
+        [Authorize]
+        // GET: Article/List
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Article/Create
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new BlogDbContext())
+                {
+                    //get authorid
+                    var authorId = db.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+
+                    article.AuthorId = authorId;
+
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+                    
+            }
+
+        }
+
+        //
+        //GET: Article/Delete
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (!IsUserAuthorisedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+        //
+        //POST: Article/Delete
+        [Authorize]
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                db.Articles.Remove(article);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        //
+        //GET: Article/Edit
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles
+                    .Where(a => a.Id == id)
+                    .First();
+
+                if (!IsUserAuthorisedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+
+                var model = new ArticleViewModel();
+                model.Id = article.Id;
+                model.Title = article.Title;
+                model.Content = article.Content;
+
+                return View(model);
+            }
+        }
+
+        //
+        //POST: Article/Edit
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel  model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new BlogDbContext())
+                {
+                    var article = db.Articles
+                        .FirstOrDefault(m => m.Id == model.Id);
+
+                    article.Title = model.Title;
+                    article.Content = model.Content;
+
+                    db.Entry(article).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        private bool IsUserAuthorisedToEdit(Article article)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
---
+++ Blog/Blog/Models/BlogDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Blog.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
+
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual IDbSet<Article> Articles { get; set; }
+
+        public BlogDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+    }
+}
